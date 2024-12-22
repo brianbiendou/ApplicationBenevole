04a46a8b718e089f96cceab1149d66f0c0c8219a
BaptisteHenriet <bapt.henriet@gmail.com>
2024-12-22 22:05:57 +0100

    Test correction classTest

--- UserService/src/test/java/fr/insa/mas/UserService/UserServiceApplicationTests.java
+++ UserService/src/test/java/fr/insa/mas/UserService/UserServiceApplicationTests.java
@@ -2,8 +2,10 @@ package fr.insa.mas.UserService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = { "spring.config.location=classpath:../.github/workflows/main_userservice1.yml" })
 class UserServiceApplicationTests {
 
 	@Test
